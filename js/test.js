0a0bbd3d08d78e03b57f3ed552c5d78d4085f0f4
Pierre Luca <pierreluca.gentile@gmail.com>
2016-11-29 13:07:39 +0100

    initial project version

---
+++ js/test.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+	/*
+	$(window).on("scroll", function() {
+	   
+	   var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+           shrinkOn = 200;
+        
+        if (distanceY > shrinkOn) {
+            ...
+        } else {
+            ...
+        } 
+	    
+	});
+    */
+
+
+});
