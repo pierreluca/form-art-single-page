2936935633d0c8e34175039ae2fccbc165ca5a4d
Pierre Luca <pierreluca.gentile@gmail.com>
2016-12-04 01:54:52 +0100

    Jquery - header bullet menu - hide show

---
+++ js/headerBulletMenu__hide-show.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+	
+	var sensPos = 0;
+
+    $( window ).on("scroll", function() {
+
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+
+        if($(window).width() < 960) {
+
+            if (sensPos > distanceY && distanceY > 40) {
+                sensPos = distanceY--;
+                $('.nav-bullet').css('margin-top','-124px');
+                $('.nav-main').css('margin-top','0');
+            } else {
+                sensPos = distanceY++;
+                $('.nav-bullet').css('margin-top','0');
+                $('.nav-main').css('margin-top','-124px');
+            }
+
+        }
+
+	});
+
+});
+
